935fa78e2b75eda23feb20f6000a771ca061d7ce
learningpath2025 <learningpath2025@outlook.com>
2025-09-30 11:40:03 -0500

    adding different method

--- Program.cs
+++ Program.cs
@@ -39,6 +39,48 @@
             int[] nums = { -1, 1, 0, -3, 3 };
             DisplayArray(nums);
             DisplayArray(ProductExceptFirstElement(nums));
+            //ListMultiplication();
+
+        }
+
+
+        public static void ListMultiplication()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4 };
+            numbers.Remove(0);
+            List<int> results = new List<int>();
+            List<int> distinctResults = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (i != j) // Ensure unique elements for multiplication
+                    {
+                        results.Add(numbers[i] * numbers[j]);
+                    }
+                }
+            }
+
+            Console.WriteLine("Multiplication results:");
+            foreach (int result in results.Distinct())
+            {
+                distinctResults.Add(result);
+                //Console.Write(result + " ");
+            }
+
+            int a = distinctResults.Count/2;
+            int b = 1;
+            for (int i = 0; i < a; i++)
+            {
+                b *= distinctResults[i];
+            }
+            distinctResults.RemoveRange(0, a);
+            distinctResults.Add(b);
+            distinctResults.Reverse();
+
+            DisplayArray(distinctResults.ToArray());
+            Console.WriteLine();
         }
     }
 }
